e733f27024efe5e08c40ed88b8d40e2a2ad93f98
fredo <fred@autogrif.com>
2018-10-28 23:44:43 +0100

    edit film link

--- src/components/Dvd.js
+++ src/components/Dvd.js
@@ -5,13 +5,31 @@ class Dvd extends Component {
   constructor(props){
     super(props);
     this.state = {dvd:props};
+    this.handleChange = this.handleChange.bind(this);
+    this.zoneList = [];
+    for(let i=1;i<4;i++){
+      this.zoneList.push(i);
+    }
   }
 
+  handleChange(event) {
+      console.log('event.target.name='+event.target.name);
+      console.log('event.target.value='+event.target.value);
+      this.setState({ [event.target.name]: event.target.value });
+  }
   render() {
+    const {dvd} = this.state.dvd;
+    console.log('dvd.zone='+dvd.zone);
     return(
       <div className="form-group">
         <label>Zone DVD
-          <input type="text" id="zone_dvd"  className="form-control" defaultValue={this.props.dvd.zone}/>
+          <select name="dvd_zone" value={dvd.zone} onChange={this.handleChange}>
+          {
+            this.zoneList.map((zone)=>{
+              return <option value={zone}>{zone}</option>
+            })
+          }
+          </select>
         </label>
       </div>
     )
--- src/components/EditFilm.js
+++ src/components/EditFilm.js
@@ -5,6 +5,13 @@ class EditFilm extends Component {
   constructor(props){
     super(props);
     this.state = {film:null,err:null,isLoaded: false};
+    this.handleChange = this.handleChange.bind(this);
+    this.anneeList = [];
+    var currentTime = new Date();
+    var yyyy = currentTime.getFullYear();
+    for(let i=yyyy;i>1930;i--){
+      this.anneeList.push(i);
+    }
   }
 
   componentDidMount(){
@@ -22,40 +29,48 @@ class EditFilm extends Component {
     }
     )
   }
+  handleChange(event) {
+      this.setState({ [event.target.name]: event.target.value });
+  }
 
   render() {
     const {film,err,isLoaded } = this.state;
-    console.log('this.props.match.params.filmId='+this.props.match.params.filmId);
-
     if (!isLoaded) {
       return <div className="container-fluid text-center">Loading...</div>;
     }else{
-    return(
-      <div className="container-fluid text-center">
-      <form id="principal">
-        <div className="row justify-content-md-center">
-          <div className="col-md-2 col-md-offset-5 text-center">
-            <div className="form-group">
-              <label>Titre
-                <input type="text" id="titre"  className="form-control" defaultValue={film.titre}/>
-              </label>
+      //console.log('this.props.match.params.filmId='+this.props.match.params.filmId);
+      return(
+        <div className="container-fluid text-center">
+        <form id="principal">
+          <div className="row justify-content-md-center">
+            <div className="col-md-2 col-md-offset-5 text-center">
+              <div className="form-group">
+                <label>Titre
+                  <input type="text" id="titre" className="form-control" defaultValue={film.titre}/>
+                </label>
+              </div>
+              <div className="form-group">
+                <label>Titre Original
+                  <input type="text" id="titreO" className="form-control" defaultValue={film.titreO==null?'':film.titreO}/>
+                </label>
+              </div>
+              <div className="form-group">
+                <label>Année
+                  <select id="annee" defaultValue ={film.annee} onChange={film.anneee}>
+                  {
+                    this.anneeList.map((annee)=>{
+                      return <option key={annee} value={annee}>{annee}</option>
+                    })
+                  }
+                  </select>
+                </label>
+              </div>
+              <Dvd dvd={film.dvd}/>
             </div>
-            <div className="form-group">
-              <label>Titre Original
-                <input type="text" id="titreO"  className="form-control" defaultValue={film.titreO==null?'':film.titreO}/>
-              </label>
-            </div>
-            <div className="form-group">
-              <label>Année
-                <input type="text" id="annee"  className="form-control" defaultValue={film.annee}/>
-              </label>
-            </div>
-            <Dvd dvd={film.dvd}/>
           </div>
+        </form>
         </div>
-      </form>
-      </div>
-    )
+      )
   }
   }
 }
